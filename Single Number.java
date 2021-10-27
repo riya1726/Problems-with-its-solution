b24a720130124828214ecff5e8e4771321f306c2
Riya <55021104+riya1726@users.noreply.github.com>
2021-10-27 09:41:40 +0530

    Create Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for(int i=0; i< nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] = 0;
+                nums[i+1] = 0;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] !=0){
+                res =  nums[i];
+            }
+        }
+        return res;
+    }
+}
+
+Another Approach :-
+ 
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> hset = new HashSet<>();
+        for(int ele:nums){
+            if(hset.contains(ele)){
+                hset.remove(ele);
+            }
+            else{
+                hset.add(ele);
+            }
+        }
+        for(int ele:hset){
+            return ele;
+        }
+        return -1;
+    }
+}
+
+Another Approach :-
+  
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++) {
+            if((nums[i] ^ nums[i+1]) == 0){
+                i++;
+            }
+            else{
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
